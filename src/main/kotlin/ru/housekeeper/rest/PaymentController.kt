e2ac657795eb87afca2c0fc20f16895004acd0ae
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2023-05-19 11:03:06 +0300

    get all incoming payments for a company with a filter

--- src/main/kotlin/ru/housekeeper/rest/PaymentController.kt
+++ src/main/kotlin/ru/housekeeper/rest/PaymentController.kt
@@ -1,18 +1,21 @@
 package ru.housekeeper.rest
 
 import io.swagger.v3.oas.annotations.Operation
+import org.springframework.data.domain.Page
 import org.springframework.web.bind.annotation.*
 import ru.housekeeper.model.dto.AnnualPaymentVO
 import ru.housekeeper.model.dto.PaymentVO
+import ru.housekeeper.model.filter.CompanyPaymentsFilter
+import ru.housekeeper.service.CounterpartyService
 import ru.housekeeper.service.PaymentService
-import ru.housekeeper.utils.getContractNumberFromDepositPurpose
-import ru.housekeeper.utils.toPaymentVO
+import ru.housekeeper.utils.*
 
 @CrossOrigin
 @RestController
 @RequestMapping("/payments")
 class PaymentController(
     private val paymentService: PaymentService,
+    private val counterpartyService: CounterpartyService,
 ) {
 
     @Operation(summary = "Find all deposits made (outgoing payments)")
@@ -31,4 +34,14 @@ class PaymentController(
         @PathVariable year: Int,
     ): AnnualPaymentVO = paymentService.findAnnualPayments(year)
 
+
+    @PostMapping(path = ["/counterparties/{inn}/incoming-payments"])
+    @Operation(summary = "Find payments from the company")
+    fun findPaymentsFromCompany(
+        @RequestParam(value = "pageNum", required = false, defaultValue = "0") pageNum: Int,
+        @RequestParam(value = "pageSize", required = false, defaultValue = "10") pageSize: Int,
+        @PathVariable inn: String,
+        @RequestBody filter: CompanyPaymentsFilter,
+    ): Page<PaymentVO> = counterpartyService.findAllFromCompanyByFilter(inn, pageNum, pageSize, filter)
+        .toIncomingPaymentResponse(pageNum, pageSize)
 }
