15f6e268d258100269e26aeb69b86e4056250e60
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2023-05-24 20:09:41 +0300

    Add new API: get types of room

--- src/main/kotlin/ru/housekeeper/rest/RoomController.kt
+++ src/main/kotlin/ru/housekeeper/rest/RoomController.kt
@@ -3,6 +3,7 @@ package ru.housekeeper.rest
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.data.domain.Page
 import org.springframework.web.bind.annotation.*
+import ru.housekeeper.enums.RoomTypeEnum
 import ru.housekeeper.excel.*
 import ru.housekeeper.model.dto.RoomVO
 import ru.housekeeper.model.filter.RoomFilter
@@ -16,6 +17,14 @@ class RoomController(
     private val roomService: RoomService,
 ) {
 
+    @GetMapping("/types")
+    @Operation(summary = "Get all types of room")
+    fun getRoomTypes(): List<RoomTypeResponse> = RoomTypeEnum.values().map { RoomTypeResponse(it.name, it.description) }
+
+    data class RoomTypeResponse(
+        val name: String,
+        val description: String,
+    )
 
     @PostMapping
     @Operation(summary = "Get all rooms with filter")
