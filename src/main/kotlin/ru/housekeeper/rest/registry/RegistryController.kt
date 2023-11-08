2b937ed700363f39e41e5d95dae83412f0d3d8c3
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2023-11-08 15:08:17 +0300

    check commit 3

--- src/main/kotlin/ru/housekeeper/rest/registry/RegistryController.kt
+++ src/main/kotlin/ru/housekeeper/rest/registry/RegistryController.kt
@@ -1,11 +1,15 @@
 package ru.housekeeper.rest.registry
 
 import io.swagger.v3.oas.annotations.Operation
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import ru.housekeeper.service.registry.RegistryService
+import ru.housekeeper.utils.logger
+import ru.housekeeper.utils.yyyyMMddHHmmssDateFormat
+import java.nio.charset.Charset
+import java.time.LocalDateTime
 
 @CrossOrigin
 @RestController
@@ -19,18 +23,16 @@ class RegistryController(
     @GetMapping
     @Operation(summary = "Check or create new registry")
     fun getRegistry(
-//        @RequestBody filter: RegistryFilter,
-    ): Int {
-        return 42
-//        val registry = registryService.make(filter.bankAccount)
-//        logger().info("Registry size: ${registry.size}")
-//        val fileName = "${LocalDateTime.now().format(yyyyMMddHHmmssDateFormat())}_registry.txt"
-//        return registry.size
-//        return ResponseEntity.ok()
-//            .contentType(MediaType.APPLICATION_OCTET_STREAM)
-//            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$fileName\"")
-//            .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION)
-//            .body(registry.joinToString(separator = "\r\n").toByteArray(Charset.forName("WINDOWS-1251")))
+        @RequestBody filter: RegistryFilter,
+    ): ResponseEntity<ByteArray> {
+        val registry = registryService.make(filter.bankAccount)
+        logger().info("Registry size: ${registry.size}")
+        val fileName = "${LocalDateTime.now().format(yyyyMMddHHmmssDateFormat())}_registry.txt"
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$fileName\"")
+            .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION)
+            .body(registry.joinToString(separator = "\r\n").toByteArray(Charset.forName("WINDOWS-1251")))
     }
 
 }
