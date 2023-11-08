75ae94ebf5c984741f5ccb6afacfdb0c2cf4fadb
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2023-11-08 14:36:31 +0300

    check commit

--- src/main/kotlin/ru/housekeeper/rest/registry/RegistryController.kt
+++ src/main/kotlin/ru/housekeeper/rest/registry/RegistryController.kt
@@ -1,13 +1,10 @@
 package ru.housekeeper.rest.registry
 
 import io.swagger.v3.oas.annotations.Operation
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import ru.housekeeper.service.registry.RegistryService
+import ru.housekeeper.utils.logger
 import ru.housekeeper.utils.yyyyMMddHHmmssDateFormat
-import java.nio.charset.Charset
 import java.time.LocalDateTime
 
 @CrossOrigin
@@ -23,14 +20,16 @@ class RegistryController(
     @Operation(summary = "Check or create new registry")
     fun getRegistry(
         @RequestBody registryFilter: RegistryFilter,
-    ): ResponseEntity<ByteArray> {
+    ): Int {
         val registry = registryService.make(registryFilter.bankAccount)
+        logger().info("Registry size: ${registry.size}")
         val fileName = "${LocalDateTime.now().format(yyyyMMddHHmmssDateFormat())}_registry.txt"
-        return ResponseEntity.ok()
-            .contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$fileName\"")
-            .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION)
-            .body(registry.joinToString(separator = "\r\n").toByteArray(Charset.forName("WINDOWS-1251")))
+        return registry.size
+//        return ResponseEntity.ok()
+//            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+//            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$fileName\"")
+//            .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION)
+//            .body(registry.joinToString(separator = "\r\n").toByteArray(Charset.forName("WINDOWS-1251")))
     }
 
 }
