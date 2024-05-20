2d8d675ab0bcba711d8ec83a33dc18b6db96399e
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2024-05-21 00:36:22 +0300

    add V6 for sber registry

--- src/main/kotlin/ru/housekeeper/parser/PaymentParser.kt
+++ src/main/kotlin/ru/housekeeper/parser/PaymentParser.kt
@@ -24,6 +24,7 @@ class PaymentParser(private val file: MultipartFile) {
         if (version.startsWith("СберБизнес. 03.001.02-18")) return V3
         if (version.startsWith("СберБизнес. 03.001.02-20")) return V4
         if (version.startsWith("СберБизнес. 03.001.02-21")) return V5
+        if (version.startsWith("СберБизнес. 03.001.02-22")) return V6
         throw IllegalArgumentException("Unknown version of excel document")
     }
 
@@ -43,7 +44,7 @@ class PaymentParser(private val file: MultipartFile) {
 
     private fun sheetParser(sheet: Sheet, version: SberVersionEnum): List<PaymentVO> {
         val dateNum = when (version) {
-            V1, V3, V4, V5 -> 1
+            V1, V3, V4, V5, V6 -> 1
             V2 -> 2
         }
         val payerNum = 4
@@ -54,7 +55,7 @@ class PaymentParser(private val file: MultipartFile) {
         val voNum = 16
         val bikAndNameNum = 17
         val purposeNum = when (version) {
-            V1, V3, V4, V5 -> 20
+            V1, V3, V4, V5, V6 -> 20
             V2 -> 19
         }
 
@@ -72,10 +73,10 @@ class PaymentParser(private val file: MultipartFile) {
             val recipient = counterpartyParser(row.getCell(recipientNum).stringCellValue.trim())
             val (bik, bankName) = when (version) {
                 V1 -> bikAndNameParser(row.getCell(bikAndNameNum).stringCellValue.trim())
-                V2, V3, V4, V5 -> bikAndNameParserV2orV3(row.getCell(bikAndNameNum).stringCellValue.trim())
+                V2, V3, V4, V5, V6 -> bikAndNameParserV2orV3(row.getCell(bikAndNameNum).stringCellValue.trim())
             }
             val date = when (version) {
-                V1, V4, V5 -> row.getCell(dateNum).localDateTimeCellValue
+                V1, V4, V5, V6 -> row.getCell(dateNum).localDateTimeCellValue
                 V2, V3 -> LocalDate.parse(
                     row.getCell(dateNum).stringCellValue.toString().trim(),
                     DateTimeFormatter.ofPattern("dd.MM.yyyy")
