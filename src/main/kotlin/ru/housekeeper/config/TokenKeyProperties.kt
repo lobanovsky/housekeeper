f854dc41cf2a44c9cdb81a31c2ddf9644b4bc744
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2024-10-17 14:57:29 +0300

    implicitly

--- src/main/kotlin/ru/housekeeper/config/TokenKeyProperties.kt
+++ src/main/kotlin/ru/housekeeper/config/TokenKeyProperties.kt
@@ -3,6 +3,7 @@ package ru.housekeeper.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 
+//AUTH_TOKEN_KEY env variable as a secret key for JWT
 @Component
 @ConfigurationProperties("auth.token")
 class TokenKeyProperties {
