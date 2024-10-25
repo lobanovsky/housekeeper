b129bfac74594d91e9771d199af25fac3ec0564d
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2024-10-25 09:09:18 +0300

    add the color for the workspace in the user profile

--- src/main/kotlin/ru/housekeeper/model/response/UserResponse.kt
+++ src/main/kotlin/ru/housekeeper/model/response/UserResponse.kt
@@ -3,6 +3,7 @@ package ru.housekeeper.model.response
 import ru.housekeeper.model.entity.User
 import ru.housekeeper.model.entity.Workspace
 import ru.housekeeper.rest.UserController
+import ru.housekeeper.utils.color
 import java.time.LocalDateTime
 import kotlin.collections.map
 import kotlin.collections.toSet
@@ -21,7 +22,8 @@ data class UserResponse(
 
 data class AvailableWorkspaceResponse(
     val id: Long,
-    val name: String
+    val name: String,
+    val color: String
 )
 
 fun User.toResponse(workspaces: Collection<Workspace>) = UserResponse(
@@ -38,5 +40,6 @@ fun User.toResponse(workspaces: Collection<Workspace>) = UserResponse(
 
 fun Workspace.toAvailableWorkspaceResponse() = AvailableWorkspaceResponse(
     id = id,
-    name = name
+    name = name,
+    color = name.color()
 )
