700cb82d59e00a9a61f343293249ac7cfb435af8
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2025-02-26 20:46:57 +0300

    load debt

--- src/main/kotlin/ru/housekeeper/rest/DebtController.kt
+++ src/main/kotlin/ru/housekeeper/rest/DebtController.kt
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import ru.housekeeper.service.DebtService
 
+/**
+ * Формирование претензий к должникам по капитальному ремонту
+ * 1. Загрузка долгов из Excel в базу данных
+ * 2. Генерация претензий на основании долгов из БД + шаблон претензии в формате Word
+ */
 @CrossOrigin
 @RestController
 @RequestMapping("/debts")
