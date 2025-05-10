5c1e379c01796dfb1391903d6cc2c9471d9beae8
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2025-05-10 18:06:14 +0300

    Update JWT expiration time to 10 minutes

--- src/main/kotlin/ru/housekeeper/service/JwtService.kt
+++ src/main/kotlin/ru/housekeeper/service/JwtService.kt
@@ -39,7 +39,7 @@ class JwtService(keyProperties: TokenKeyProperties) {
         .claims(buildMap(claimsBuilder))
         .subject(user.email)
         .issuedAt(Date())
-        .expiration(LocalDateTime.now().plusMinutes(1).asDate())
+        .expiration(LocalDateTime.now().plusMinutes(10).asDate())
         .signWith(secretKey, HS256)
         .compact()
 
