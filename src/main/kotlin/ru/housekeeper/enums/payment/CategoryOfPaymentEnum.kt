cd948ae144a4991ae4bdcdc73f4a0dd7feeb0139
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2026-01-13 00:58:45 +0300

    Add OVERHAUL category to CategoryOfPaymentEnum with Russian description

--- src/main/kotlin/ru/housekeeper/enums/payment/CategoryOfPaymentEnum.kt
+++ src/main/kotlin/ru/housekeeper/enums/payment/CategoryOfPaymentEnum.kt
@@ -75,6 +75,8 @@ enum class CategoryOfPaymentEnum(val description: String) {
 
     DESIGN_DOCUMENTATION("Проектная документация"),
 
+    OVERHAUL("Капитальный ремонт"),
+
     OTHER("Остальное"),
 
     UNKNOWN("Неизвестно")
