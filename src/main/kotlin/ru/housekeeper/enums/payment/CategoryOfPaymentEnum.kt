9cb8f2a1c68d2fcb7934165e458285741dc7790e
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2026-01-13 00:34:31 +0300

    Add CLEANING category to CategoryOfPaymentEnum with Russian description

--- src/main/kotlin/ru/housekeeper/enums/payment/CategoryOfPaymentEnum.kt
+++ src/main/kotlin/ru/housekeeper/enums/payment/CategoryOfPaymentEnum.kt
@@ -63,6 +63,8 @@ enum class CategoryOfPaymentEnum(val description: String) {
 
     CLEANING_PARKING("Уборка паркинга"),
 
+    CLEANING("Уборка"),
+
     AIR_FILTERS("Воздушные фильтры"),
 
     OPERATIONAL_TESTS("Эксплуатационные испытания"),
